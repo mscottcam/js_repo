ee9d14c69554865802c3f6bfee50bb0e25d93659
mscottcam <mscottcam@gmail.com>
2017-06-12 14:22:37 -0500

    yearOfBirth now separate function

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function whoAmI(name, age) {
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  const yearOfBirth = 2017 - age;
-  console.log(`I was born in ${yearOfBirth}`);
+function yearOfBirth(age) {
+  return 2017 - age;
 }
 
-whoAmI(Scott, 29);
+function whoAmI(name, age) {
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  var yob = yearOfBirth(29);
+  console.log(`I was born in ${yob}`);
+}
+
+yearOfBirth(29);
+whoAmI('Scott', 29);
