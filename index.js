8857d7470f7bceeba0f62840b89a004b3cbff24f
mscottcam <mscottcam@gmail.com>
2017-06-12 14:14:05 -0500

    added yearOfBirth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age) {
-  console.log(`'Hi, my name is ${name} and I'm ${age} years old'`);
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  const yearOfBirth = 2017 - age;
+  console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI(Scott, 29);
